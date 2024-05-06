1997aff460c5661a014a525cbaed4d6ce3b6a4b2
AlvaroAVR <alvaro20vr@gmail.com>
2024-05-06 16:14:25 -0400

    agregados metodos get

---
+++ v1/parcela_tipo/index.php
@@ -0,0 +1,43 @@
+<?php
+include_once '../version1.php';
+
+
+//valores de los parametros
+$existeId = false;
+$valorId = 0;
+
+if (count($_parametros) > 0) {
+    foreach ($_parametros as $p) {
+        if (strpos($p, 'id') !== false) {
+            $existeId = true;
+            $valorId = explode('=', $p)[1];
+        }
+    }
+}
+
+if($_version == 'v1'){
+    if($_mantenedor == 'parcela_tipo'){
+        switch ($_metodo){
+            case 'GET':
+                if ($_header == $_token_get){
+                    
+                    //DE ESTA FORMA SE CREA UNA CONEXION CON LA BASE DE DATOS PARA UTILIZAR LA INFORMACION QUE ESTA CONTIENE
+                    include_once 'controller.php';
+                    include_once '../conexion.php';
+                    $control = new Controlador();
+                    $lista = $control->getAll();
+                    
+                    http_response_code(200);
+                    echo json_encode(["data" => $lista]);
+                }else{
+                    http_response_code(401);
+                    echo json_encode(["Error" => "No tiene autorizacion GET"]);
+                }
+                break;
+            default:
+                http_response_code(405);
+                echo json_encode(["Error" => "No implementado"]);
+                break;
+        }
+    }
+}
